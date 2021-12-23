fc4c86dfee9b1f86fd8166487e067e6d48481365
Zahra <mirzakizahra@gmail.com>
2021-12-23 13:46:22 -0800

    add addTicket option to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ package main.java;
 import main.java.dto.Manegement;
 import main.java.entity.Manager;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -33,6 +36,20 @@ public class Main {
                                     ,scanner.nextInt());
                                     break;
                                 case 2:
+                                    System.out.println("please enter date");
+                                    String date= scanner.next();
+                                    Date date2=new Date();
+                                    SimpleDateFormat formatter=
+                                            new SimpleDateFormat("dd/MMM/yyyy HH:mm");
+                                    try {
+                                        date2=formatter.parse(date);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    System.out.println("please enter count,barcode" +
+                                            ",originCity,destinationCity,plaque");
+                                    manegement.createTicket(scanner.nextInt(),scanner.nextInt(),date2
+                                    ,scanner.next(),scanner.next(),scanner.nextInt());
                                     break;
                                 case 3:
                                     break;
